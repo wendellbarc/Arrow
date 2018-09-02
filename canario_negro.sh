65c2fb728a2715d3384d27880ed2e256f751c597
Wendell B <wendellbarc@gmail.com>
2018-09-02 13:44:18 +0000

    alteracao

--- canario_negro.sh
+++ canario_negro.sh
@@ -4,3 +4,7 @@ echo "Script de teste"
 cowsay -f tux $(fortune)
 sleep 3
 
+
+ping -c2 192.168.99.20 >- && echo "Com conexao" || echo "Sem conexao"
+ping -c2 192.168.99.80 >- && echo "Com conexao" || echo "Sem conexao"
+
